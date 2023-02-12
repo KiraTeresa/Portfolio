16a4131e55655428f12ef15574bb7f307d9aaca5
Kira <kira.soba@gmx.de>
2023-02-12 19:19:15 +0100

    Adding App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import './App.css';
-import Navbar from "./components/Navbar/Navbar";
+import "./app.scss"
+import Navbar from "./components/Navbar/Navbar"
 
-import HomePage from "./pages/HomePage/HomePage";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import HomePage from "./pages/HomePage/HomePage"
+import AboutPage from "./pages/AboutPage/AboutPage"
+import ProjectsPage from "./pages/Projects/ProjectsPage"
+import ContactPage from "./pages/Contact/ContactPage"
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
+import Footer from "./components/Footer/Footer"
+import LegalNotice from "./pages/LegalNotice/LegalNotice"
+import ProjectsDetail from "./pages/Projects/ProjectsDetail"
+import Loading from "./components/Loading/Loading"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
-      <Routes>
-        <Route  path="/" element={<HomePage />} />
+      <div className="main-container">
+        <Routes className="shape">
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:slug" element={<ProjectsDetail />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/legal-notice" element={<LegalNotice />} />
 
-        <Route path="/about" element={<AboutPage />} />
+          <Route path="/loader" element={<Loading />} />
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>      
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
