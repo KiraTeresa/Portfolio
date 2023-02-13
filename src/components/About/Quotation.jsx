4be82dafad992e2d9a7dfa932c39e498b3e21a3e
Kira <kira.soba@gmx.de>
2023-02-13 07:21:29 +0100

    Removed unused css

--- src/components/About/Quotation.jsx
+++ src/components/About/Quotation.jsx
@@ -2,8 +2,8 @@ import './about.scss'
 
 function Quotation({ isVisible }) {
     return (
-        <section className={`quote ${isVisible ? 'visible' : ''}`}>
-            <div>
+        <section className={`${isVisible ? 'visible' : ''}`}>
+            <div className='quote'>
                 “Becoming a programmer means to choose a path of continuous learning opportunities. As I am a person who likes taking on challenges and to expand my knowledge, getting into tech was the right way for me.”
             </div>
         </section>
