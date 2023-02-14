636de3be7f4ca0ed69dc58bdf6e1cde0b88430ae
Kira <kira.soba@gmx.de>
2023-02-14 07:19:54 +0100

    Add placeholder component for contact page

---
+++ src/components/Contact/ContactConnect.jsx
@@ -0,0 +1,14 @@
+function ContactConnect() {
+  return (
+    <section className="connect">
+      <h2>Get in touch</h2>
+      <div>
+        <a href="https://www.linkedin.com/in/kira-teresa-soba/">➽ Linkedin</a>
+        <a href="https://github.com/KiraTeresa">➽ GitHub</a>
+        <a href="mailto:kira.soba@gmx.de">➽ Email</a>
+      </div>
+    </section>
+  )
+}
+
+export default ContactConnect
--- src/pages/Contact/ContactPage.jsx
+++ src/pages/Contact/ContactPage.jsx
@@ -1,11 +1,17 @@
-import ContactForm from "../../components/Contact/ContactForm";
-import ContactHead from "../../components/Contact/ContactHead";
+import ContactConnect from "../../components/Contact/ContactConnect";
+// import ContactForm from "../../components/Contact/ContactForm";
+// import ContactHead from "../../components/Contact/ContactHead";
+import "../../components/Contact/contact.scss";
 
 function ContactPage() {
     return (
         <div className='container contact'>
-            <ContactHead />
-            <ContactForm />
+            {/* <ContactHead />
+            <ContactForm /> */}
+
+
+            {/* Placeholder component until contact form works */}
+            <ContactConnect />
         </div>
     )
 }
